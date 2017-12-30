7a76d53b50de64b027a528af7e457c6b785a3d83
罗云强 <99190092@qq.com>
2017-12-30 20:55:15 +0800

    加密

---
+++ BigBoot-common/src/test/java/com/lyq3/bigboot/common/util/JasyptTest.java
@@ -0,0 +1,23 @@
+package com.lyq3.bigboot.common.util;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class) // SpringJUnit支持，由此引入Spring-Test框架支持！
+//@SpringBootTest(classes = Application.class) // 指定我们SpringBoot工程的Application启动类
+@WebAppConfiguration // 由于是Web项目，Junit需要模拟ServletContext，因此我们需要给我们的测试类加上@WebAppConfiguration。
+public class JasyptTest {
+    @Autowired
+    StringEncryptor stringEncryptor;
+
+    @Test
+    public void encryptPwd() {
+        String result = stringEncryptor.encrypt("yourpassword");
+        System.out.println(result);
+    }
+}
