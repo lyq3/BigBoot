0e8c649e8155a65fa5087190c5751d10cc385072
罗云强 <99190092@qq.com>
2018-01-06 22:05:15 +0800

    集成代码生成器

---
+++ BigBoot-upms/BigBoot-upms-common/src/main/java/com/lyq3/bigboot/upms/common/entity/po/UpmsUser.java
@@ -0,0 +1,158 @@
+package com.lyq3.bigboot.upms.common.entity.po;
+
+import java.util.Date;
+import java.io.Serializable;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 卡卢比
+ * @since 2018-01-06
+ */
+public class UpmsUser implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+	private Integer userId;
+	private String username;
+    /**
+     * 密码（加密后）
+     */
+	private String password;
+    /**
+     * 加密盐
+     */
+	private String salt;
+    /**
+     * 真实姓名
+     */
+	private String realname;
+    /**
+     * 昵称
+     */
+	private String nickname;
+    /**
+     * 头像
+     */
+	private String avatar;
+    /**
+     * 电话
+     */
+	private String phone;
+    /**
+     * 邮箱
+     */
+	private String email;
+    /**
+     * 性别
+     */
+	private Integer sex;
+    /**
+     * 状态（1-正常，2-停用）
+     */
+	private Integer status;
+    /**
+     * 创建时间
+     */
+	private Date createtime;
+
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getSalt() {
+		return salt;
+	}
+
+	public void setSalt(String salt) {
+		this.salt = salt;
+	}
+
+	public String getRealname() {
+		return realname;
+	}
+
+	public void setRealname(String realname) {
+		this.realname = realname;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getAvatar() {
+		return avatar;
+	}
+
+	public void setAvatar(String avatar) {
+		this.avatar = avatar;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Integer getSex() {
+		return sex;
+	}
+
+	public void setSex(Integer sex) {
+		this.sex = sex;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	public Date getCreatetime() {
+		return createtime;
+	}
+
+	public void setCreatetime(Date createtime) {
+		this.createtime = createtime;
+	}
+
+}
