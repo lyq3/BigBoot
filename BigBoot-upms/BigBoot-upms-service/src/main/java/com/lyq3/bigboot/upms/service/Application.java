5282d45943bfdd4a6b0e112d24dc1005ef259679
罗云强 <99190092@qq.com>
2018-01-06 16:01:19 +0800

    集成Dubbo

---
+++ BigBoot-upms/BigBoot-upms-service/src/main/java/com/lyq3/bigboot/upms/service/Application.java
@@ -0,0 +1,20 @@
+package com.lyq3.bigboot.upms.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ComponentScan(basePackages = { "com.lyq3" })
+@ImportResource(locations = {"classpath:xmlConfig/*.xml"})
+public class Application extends SpringBootServletInitializer {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+    public static void main(String[] args) {
+        logger.info("\n===========upms-service开始启动========\n");
+        SpringApplication.run(Application.class, args);
+        logger.info("\n===========upms-service启动完成========\n");
+    }}
---
+++ BigBoot-upms/BigBoot-upms-service/src/main/java/com/lyq3/bigboot/upms/service/DubboServiceTest.java
@@ -0,0 +1,12 @@
+package com.lyq3.bigboot.upms.service;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.lyq3.bigboot.upms.api.DubboTestApi;
+
+@Service
+public class DubboServiceTest implements DubboTestApi{
+    @Override
+    public void test() {
+        System.out.println("\n==============Dubbo-Service测试成功==========\n");
+    }
+}
